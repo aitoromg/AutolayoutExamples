cc28490cf50a4b3453ae29edeaab5cb6b8818efd
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-04 17:48:37 -0300

    Initial Commit

---
+++ AutolayoutExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutolayoutExamples
+//
+//  Created by Charles Moncada on 04/10/18.
+//  Copyright © 2018 Charles Moncada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
