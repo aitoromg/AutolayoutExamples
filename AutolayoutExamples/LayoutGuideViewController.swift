8478ee04fe487834be177be8ec3b79dd2c9a8692
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-08 17:33:56 -0300

    UILayoutGuides

--- AutolayoutExamples/LayoutGuideViewController.swift
+++ AutolayoutExamples/LayoutGuideViewController.swift
@@ -12,7 +12,7 @@ class LayoutGuideViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		let images = [#imageLiteral(resourceName: "autobots@1x.png"),#imageLiteral(resourceName: "gameover@1x.png"),#imageLiteral(resourceName: "decepticons@1x.png")]
+		let images = [UIImage(named: "autobots"), UIImage(named: "decepticons"), UIImage(named: "gameover")]
 		let imageViews: [UIImageView] = images.map { image in
 			let imageView = UIImageView(image: image)
 			imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -23,16 +23,42 @@ class LayoutGuideViewController: UIViewController {
 		let spacerGuides = (0..<images.count - 1).map { _ in UILayoutGuide()}
 		let containerGuide = UILayoutGuide()
 
+		spacerGuides.forEach(view.addLayoutGuide)
+		view.addLayoutGuide(containerGuide)
+
+		containerGuide.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
 		NSLayoutConstraint.activate(
-			[ containerGuide.leadingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor),
-			  containerGuide.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor),
-			  { let constraint = spacerGuides.first!.widthAnchor.constraint(equalToConstant: 80)
-				constraint.priority = UILayoutPriority(749)
-				return constraint
+			[
+				containerGuide.leadingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor),
+				containerGuide.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor),
+				{ let constraint = spacerGuides.first!.widthAnchor.constraint(equalToConstant: 80)
+					constraint.priority = UILayoutPriority(749)
+					return constraint
 				}(),
-			  spacerGuides.last!.widthAnchor.constraint(equalTo: spacerGuides.first!.widthAnchor)
+				spacerGuides.last!.widthAnchor.constraint(equalTo: spacerGuides.first!.widthAnchor)
 			]
 		)
 
+		NSLayoutConstraint.activate(
+				imageViews.enumerated().flatMap { index, imageView in
+					[
+						imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+						imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
+						imageView.leadingAnchor.constraint(equalTo:
+							imageView == imageViews.first!
+							? containerGuide.leadingAnchor
+							: spacerGuides[index - 1].trailingAnchor
+						),
+						imageView.trailingAnchor.constraint(equalTo:
+							imageView == imageViews.last!
+								? containerGuide.trailingAnchor
+								: spacerGuides[index].leadingAnchor
+						),
+					]
+				}
+		)
+
+		imageViews.dropFirst().forEach({ $0.widthAnchor.constraint(equalTo: imageViews.first!.widthAnchor).isActive = true })
 	}
 }
